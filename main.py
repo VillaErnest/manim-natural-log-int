043b1eb2dc5b8a421c106f1fd544e76538b796c7
Ernest Louis Villacorta <villacorta.ernestlouis@gmail.com>
2024-06-14 04:37:13 +0000

    Add Introduction scence

--- main.py
+++ main.py
@@ -1,55 +1,32 @@
 from manim import *
 
-class OpeningManim(Scene):
+class Introduction(Scene):
     def construct(self):
-        title = Tex(r"This is some \LaTeX")
-        # curve = MathTex(r"\sum_{n=1}^\infty \frac{1}{n^2} = \frac{\pi^2}{6}")
-        curve = MathTex(r"y = \ln(x)")
-        VGroup(title, curve).arrange(DOWN)
+        self.camera.background_color = WHITE
+
+        title = Text(r"Natural Log Integration Trivia", color=BLACK)
+        description = Tex(r"The two areas bounded by $y = \ln(x)$ from x = 0 to x = e are equal.", color=BLACK, font_size=36)
+        curve = MathTex(r"y = \ln(x)", color=BLACK, font_size=36)
+        authors = Text(r"Authors", color=BLACK, font_size=18, weight=BOLD)
+        author_1 = Text(r"Ernest Louis Villacorta", color=BLACK, font_size=18)
+        author_2 = Text(r"Ralph Joseph Roa", color=BLACK, font_size=18)
+        author_3 = Text(r"Marc Paolo Mino", color=BLACK, font_size=18)
+
+        header = VGroup(title, description, curve).arrange(DOWN)
+        body = VGroup(authors, author_1, author_2, author_3).arrange(DOWN)
+
+        VGroup(header, body).arrange_submobjects(DOWN, buff=0.5)
+
         self.play(
             Write(title),
+            Write(description),
+            Write(authors),
+            Write(author_1),
+            Write(author_2),
+            Write(author_3),
             FadeIn(curve, shift=DOWN),
         )
+
         self.wait()
 
-        transform_title = Tex("That was a transform")
-        transform_title.to_corner(UP + LEFT)
-        self.play(
-            Transform(title, transform_title),
-            LaggedStart(*[FadeOut(obj, shift=DOWN) for obj in curve]),
-        )
-        self.wait()
 
-        grid = NumberPlane()
-        grid_title = Tex("This is a grid", font_size=72)
-        grid_title.move_to(transform_title)
-
-        self.add(grid, grid_title)  # Make sure title is on top of grid
-        self.play(
-            FadeOut(title),
-            FadeIn(grid_title, shift=UP),
-            Create(grid, run_time=3, lag_ratio=0.1),
-        )
-        self.wait()
-
-        grid_transform_title = Tex(
-            r"That was a non-linear function \\ applied to the grid"
-        )
-        grid_transform_title.move_to(grid_title, UL)
-        grid.prepare_for_nonlinear_transform()
-        self.play(
-            grid.animate.apply_function(
-                lambda p: p
-                          + np.array(
-                    [
-                        np.sin(p[1]),
-                        np.sin(p[0]),
-                        0,
-                    ]
-                )
-            ),
-            run_time=3,
-        )
-        self.wait()
-        self.play(Transform(grid_title, grid_transform_title))
-        self.wait()
