1ecd9e2bde365cbbcf01dd5d827ae852eb8f4db1
Ernest Louis Villacorta <villacorta.ernestlouis@gmail.com>
2024-06-17 02:37:18 +0000

    Add package

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 sudo apt update
 sudo apt upgrade -y
-sudo apt install -y build-essential python3-dev libcairo2-dev libpango1.0-dev ffmpeg texlive-full
+sudo apt install -y build-essential python3-dev libcairo2-dev libpango1.0-dev pkg-config python3-dev ffmpeg texlive-full
 pip3 install manim
 pip3 install -r requirements.txt
